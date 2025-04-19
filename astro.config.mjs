8d0ca56eef162911cb850e00fe47ddfcdb1a6f24
Saumya Rawal <114496132+technoprogramming92@users.noreply.github.com>
2025-04-19 12:21:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter:vercel({}) 
 });
